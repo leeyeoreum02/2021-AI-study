949ba38133fddc2d1ffa41a2452d0ede793666af
leeyeoreum02 <leeyeoreum01@gmail.com>
2021-02-19 18:55:06 +0900

    2/19 update -2

--- digit-recognizer.py
+++ digit-recognizer.py
@@ -33,4 +33,38 @@ else:
 train_df = pd.read_csv('../digit-recognizer/train.csv')
 test_df = pd.read_csv('../digit-recognizer/test.csv')
 
-train_df.head()
+Y = train_df.label.values
+X = train_df.loc[:, train_df.columns != 'label'].values / 255
+X_test = test_df.values / 255
+
+train_x, val_x, train_y, val_y = train_test_split(X, Y, test_size=0.2, random_state=42)
+
+trainTorch_x = torch.from_numpy(train_x).type(torch.cuda.FloatTensor)
+trainTorch_y = torch.from_numpy(train_y).type(torch.cuda.LongTensor)
+valTorch_x = torch.from_numpy(val_x).type(torch.cuda.FloatTensor)
+valTorch_y = torch.from_numpy(val_y).type(torch.cuda.LongTensor)
+testTorch_x = torch.from_numpy(np.array(X_test)).type(torch.cuda.FloatTensor)
+
+train = torch.utils.data.TensorDataset(trainTorch_x, trainTorch_y)
+val = torch.utils.data.TensorDataset(valTorch_x, valTorch_y)
+test = torch.utils.data.TensorDataset(testTorch_x)
+
+train_loader = DataLoader(train, batch_size=100, shuffle=False)
+val_loader = DataLoader(val, batsh_dize=100, shuffle=False)
+test_loader = DataLoader(test, batch_size=100, shuffle=False)
+
+randomlist = []
+for i in range(0, 9):
+    n = random.randint(0, len(X))
+    randomlist.append(n)
+
+fig = plt.figure(figsize=(15, 8))
+gs1 = gridspec.Gripspec(3, 3)
+axs = []
+
+for num in range(len(randomlist)):
+    axs.append(fig.add_subplot(gs1[num1 - 1]))
+    axs[-1].imshow(X[randomlist[num]].reshape(28, 28))
+    axs[-1].set_title("Actual: " + str(Y[randomlist[num]]))
+fig.subplots_adjust(hspace=0.3)
+plt.show()
