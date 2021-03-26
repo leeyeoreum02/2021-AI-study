8e3fd167741b18b3e5a2357b28794bcb70dca6c5
leeyeoreum02 <leeyeoreum01@gmail.com>
2021-03-26 11:45:16 +0900

    3/26 update

--- coco_visualize_2.py
+++ coco_visualize_2.py
@@ -7,6 +7,8 @@ import matplotlib.pyplot as plt
 from typing import Dict, Tuple, Any, List, Union
 import numpy as np
 
+import random
+
 
 class CocoDataset:
     def __init__(self, path: os.PathLike) -> None:
@@ -24,7 +26,7 @@ class CocoDataset:
                     'id': v['id'],
                     'category_id': self.categories.get(v['category_id']),
                     'bbox': bbox,
-                    'segmentation': np.array(v['segmentation'])
+                    'segmentation': np.array(v['segmentation'], dtype=object)
                 }
             )
     def __getitem__(self, index: int) -> Tuple[str, List[Dict[str, Any]]]:
@@ -41,22 +43,25 @@ class CocoDataset:
 def draw_bbox(
     image: np.ndarray,
     annotations: List[Dict[str, Any]],
-    colors: Dict[str, Tuple[np.ndarray]]
+    colors: Dict[str, Tuple[int]]
     ) -> np.ndarray:
     for v in annotations:
         bbox = v['bbox']
         x, y, w, h = list(map(round, bbox))
-        cv2.rectangle(image, (x, y), (x + w, y + h), colors[v['category_id']], 3)
-        cv2.putText(image, v['category_id'], (x - 3, y - 3), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2)
+        cv2.rectangle(image, (x, y), (x + w, y + h), colors[v['category_id']], 2)
+        cv2.putText(image, v['category_id'], (x + 1, y + 12), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2)
 
     return image
 
 def draw_masks(
     image: np.ndarray,
     annotations: List[Dict[str, Any]],
-    colors: Dict[str, Tuple[np.ndarray]]
+    colors: Dict[str, Tuple[int]]
     ) -> np.ndarray:
     for v in annotations:
+        # print('\nannotations:', annotations)
+        # print('\nv', v)
+        # print("\nv['segmentation']:", v['segmentation'])
         for seg in v['segmentation']:
             x_segs = [round(loc) for i, loc in enumerate(seg)
                         if i % 2 == 0]
@@ -65,30 +70,63 @@ def draw_masks(
             zip_segs = list(zip(x_segs, y_segs))
             zip_segs = np.array(zip_segs)
 
-            cv2.fillConvexPoly(image, zip_segs, colors[v['category_id']])
-            # cv2.addWeighted(image, 1, target, 0.5, 0)
+            mask = np.zeros_like(image)
+            mask = cv2.fillPoly(mask, [zip_segs], colors[v['category_id']])
+            image = cv2.addWeighted(image, 1, mask, 0.8, 0)
             
-
     return image
 
-def get_colors(annotations: List[Dict[str, Any]]) -> Dict[str, Tuple[np.ndarray]]:
+def get_colors(annotations: List[Dict[str, Any]]) -> Dict[str, Tuple[int]]:
     return {v['category_id']: tuple(map(int, np.random.randint(0, 255, 3))) for v in annotations}
+
+def show_fig_1x1(image: np.ndarray, file_name: str) -> None:
+    plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+    plt.title(file_name)
+    plt.axis('off')
+    plt.show()
+    plt.savefig('coco_random_idx_1x1.png')
+
+def show_fig_4x4(images: List[np.ndarray], file_names: List[str]) -> None:
+    fig = plt.figure()
+    rows = 2
+    cols = 2
+    i = 1
+
+    for image, file_name in list(zip(images, file_names)):
+        ax = fig.add_subplot(rows, cols, i)
+        ax.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+        ax.set_title(file_name, fontsize=10)
+        ax.axis('off')
+        i += 1
+    
+    plt.show()
+    fig.savefig('coco_random_idx_4x4.png')
     
 
 if __name__ == '__main__':
     coco = CocoDataset('../coco/annotations/instances_train2014.json')
-    print(coco[0])
+    
+    num_images = list(range(len(coco)))
+    random.shuffle(num_images)
+    random_idxs = num_images[:4]
+    print(coco[random_idxs[0]])
+    print(random_idxs)
+    
+    image_list = []
+    file_names = []
+    for idx in random_idxs:
+        file_name = coco[idx][0]
+        annotations = coco[idx][1]
+        
+        root = os.path.join('..', 'coco')
+        image_path = os.path.join(root, 'train2014', file_name)
 
-    root = os.path.join('..', 'coco')
-    image_path = os.path.join(root, 'train2014', coco[0][0])
+        image = cv2.imread(image_path)
+        colors = get_colors(annotations)
+        image = draw_masks(image, annotations, colors)
+        image = draw_bbox(image, annotations, colors)
 
-    image = cv2.imread(image_path)
-    colors = get_colors(coco[0][1])
-    image = draw_bbox(image, coco[0][1], colors)
-    image = draw_masks(image, coco[0][1], colors)
+        image_list.append(image)
+        file_names.append(file_name)
 
-    fig, ax = plt.subplots(dpi=200)
-    ax.imshow(image)
-    ax.axis('off')
-    plt.show()
-    fig.savefig('coco[0].png')
+    show_fig_4x4(image_list, file_names)
